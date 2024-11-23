395d8c950448729039309008d98f29e0ee59bc73
Jesse Otim <otimdanieljesse@gmail.com>
2024-11-23 10:34:32 +0300

    Add Tailwind and Refactor code

--- src/components/seconday/Sign.tsx
+++ src/components/seconday/Sign.tsx
@@ -1,50 +1,42 @@
-
-
-// Sign.js
 import { Form, Input, Button, Checkbox, Typography } from 'antd';
-import { GoogleOutlined } from '@ant-design/icons';
-
 const { Title, Text, Link } = Typography;
 
 const Sign = () => {
     return (
-        <Form layout="vertical" style={{ maxWidth: 400 }}>
-            <Title level={2} style={{ marginBottom: 9 }} >Sign in to your account</Title>
-            <Text style={{ marginBottom: 32, display: 'block' }}>Experience the power of networking</Text>
+        <Form layout="vertical" className="max-w-[400px]">
+            <Title level={2} className="mb-4 text-2xl font-semibold">Sign in to your account</Title>
+            <Text className="mb-8 block text-gray-600">Experience the power of networking</Text>
 
             <Form.Item
                 name="email"
                 label="Email"
-                rules={[{ message: 'Please enter your email!' }]}
-                style={{ fontWeight: 'bold' }} >
-                <Input placeholder="Enter your email" />
+                rules={[{ message: 'Please enter your email!' }]}>
+                <Input placeholder="Enter your email" className="rounded-md border-gray-300" />
             </Form.Item>
 
             <Form.Item
                 name="password"
                 label="Password"
-                rules={[{ message: 'Please enter your password!' }]}
-                // rules={[{ required: true, message: 'Please enter your password!' }]}
-                style={{ fontWeight: 'bold' }}>
-                <Input.Password placeholder="Enter your password" />
+                rules={[{ message: 'Please enter your password!' }]}>
+                <Input.Password placeholder="Enter your password" className="rounded-md border-gray-300" />
             </Form.Item>
 
             <Form.Item>
-                <Link href="#">Forgot your password?</Link>
+                <Link href="#" className="text-blue-600">Forgot your password?</Link>
             </Form.Item>
 
             <Form.Item name="remember" valuePropName="checked">
-                <Checkbox>Remember me</Checkbox>
+                <Checkbox className="text-sm">Remember me</Checkbox>
             </Form.Item>
 
             <Form.Item>
-                <Button type="primary" htmlType="submit" block>
+                <Button type="primary" htmlType="submit" block className="bg-blue-600 hover:bg-blue-700 text-white">
                     Sign in
                 </Button>
             </Form.Item>
 
-            <Text style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center' }}>
-                Don't have an account? <Link href="#" style={{marginLeft:'10px'}}>Sign up</Link>
+            <Text className="flex justify-center items-center text-sm">
+                Don't have an account? <Link href="#" className="ml-2 text-blue-600">Sign up</Link>
             </Text>
         </Form>
     );
