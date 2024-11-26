af5da99da0ce58bdf4fa0fff8ca914deebe7bf35
Jesse Otim <otimdanieljesse@gmail.com>
2024-11-26 15:06:22 +0300

    Resolve merge conflicts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/styles/**/*.css",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
