dcfcc035ae19b0663eb70d50fd2355b96406c7a9
kenyonyozi <kenyonyozibwengye@gmail.com>
2024-12-07 23:20:30 +0300

    fix merge conflicts in header

--- src/components/seconday/Header.tsx
+++ src/components/seconday/Header.tsx
@@ -26,10 +26,6 @@ const Header: React.FC<HeaderProps> = ({ onMenuClick }) => {
                 <Badge count={5} className="hidden lg:block">
                     <BellOutlined  className="text-xl" />
                 </Badge>
-<<<<<<< HEAD
-                {/* <Avatar size="default" icon={<UserOutlined />} /> */}
-=======
->>>>>>> develop
                 {/* Hamburger Menu */}
                 <Button
                     className="block lg:hidden mt-2"
