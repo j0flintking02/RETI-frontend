1f4ab7803c01a5737eeafacb271c7d6839f42d7f
kenyonyozi <kenyonyozibwengye@gmail.com>
2024-12-09 08:40:25 +0300

    fix width on login and register forms

--- src/layouts/Authentication/Login.page.tsx
+++ src/layouts/Authentication/Login.page.tsx
@@ -5,35 +5,35 @@ import viteLogo from "/vite.svg";
 const LoginPage = () => {
   return (
 
-    <div className='xl:py-10'>
-      <div className="mx-auto max-w-7xl">
+    <div className=''>
+      <div className="mx-auto max-w-screen h-screen">
         <div className="flex min-h-full flex-1">
           <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
-            <div className="mx-auto w-full max-w-sm lg:w-96">
+            <div className="mx-auto w-full max-w-md lg:w-96">
               <div>
                 <img
                   alt="Your Company"
                   src={viteLogo}
                   className="h-10 w-auto"
                 />
-                <h2 className="mt-8 text-2xl/9 font-bold tracking-tight text-gray-900">Sign in to your account</h2>
+                <h2 className="mt-4 text-2xl/9 font-bold tracking-tight text-gray-900">Sign in to your account</h2>
                 <p className="mt-2 text-sm/6 text-gray-500">
-                Experience the power of networking
+                  Experience the power of networking
                 </p>
               </div>
 
-              <div className="mt-10">
-                <div>
-                  <Sign />
-                </div>
+
+              <div>
+                <Sign />
               </div>
+
             </div>
           </div>
-          <div className="relative hidden w-0 flex-1 lg:block rounded-l-lg">
+          <div className="relative hidden w-0 flex-1 lg:block">
             <img
               alt=""
               src="https://images.unsplash.com/photo-1496917756835-20cb06e75b4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
-              className="absolute inset-0 size-full object-cover rounded-l-lg"
+              className="absolute inset-0 size-full object-cover"
             />
           </div>
         </div>
